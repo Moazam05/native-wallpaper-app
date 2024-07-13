546946b24e1645ab7309366e2cab0cbefd09dac1
Moazam05 <salmanmoazam08@gmail.com>
2024-07-13 16:05:09 +0500

    _layout file

--- app/index.js
+++ app/index.js
@@ -1,12 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar, Image, StyleSheet } from "react-native";
 import React from "react";
+import { wp, hp } from "../helpers/common";
 
 const WelcomeScreen = () => {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={require("../assets/images/welcome.png")}
+        style={styles.bgImg}
+        resizeMode="cover"
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bgImg: {
+    width: wp(100),
+    height: hp(100),
+    position: "absolute",
+  },
+});
 
 export default WelcomeScreen;
