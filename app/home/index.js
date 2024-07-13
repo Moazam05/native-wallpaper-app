3f8be997d80619765575957b1da372f82970d06f
Moazam05 <salmanmoazam08@gmail.com>
2024-07-13 18:22:36 +0500

    header complete

--- app/home/index.js
+++ app/home/index.js
@@ -2,13 +2,16 @@ import { View, Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { FontAwesome6 } from "@expo/vector-icons";
+// Custom Imports
 import { theme } from "../../constants/theme";
+import { wp, hp } from "../../helpers/common";
 
 const Home = () => {
-  const { top } = useSafeAreaInsets();
-  const paddingTop = top > 0 ? top + 10 : 30;
+  const insets = useSafeAreaInsets();
+  const paddingTop = insets.top > 0 ? insets.top + 10 : 30;
+
   return (
-    <View style={[styles.container, paddingTop]}>
+    <View style={[styles.container, { paddingTop }]}>
       {/* Header */}
       <View style={styles.header}>
         <Pressable>
@@ -30,6 +33,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     gap: 15,
+  },
+
+  header: {
+    marginHorizontal: wp(4),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: hp(4),
+    fontWeight: theme.fontWeights.semibold,
+    color: theme.colors.neutral(0.9),
   },
 });
 
