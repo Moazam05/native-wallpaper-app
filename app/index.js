51f6df830167ae549a5765176ff27dc951c65f68
Moazam05 <salmanmoazam08@gmail.com>
2024-07-13 16:16:14 +0500

    Animation added

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import { View, Text, StatusBar, Image, StyleSheet } from "react-native";
 import React from "react";
 import { wp, hp } from "../helpers/common";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeInDown } from "react-native-reanimated";
 
 const WelcomeScreen = () => {
   return (
@@ -11,6 +13,25 @@ const WelcomeScreen = () => {
         style={styles.bgImg}
         resizeMode="cover"
       />
+      {/* Linear Gradient */}
+      <Animated.View
+        entering={FadeInDown.duration(600)}
+        style={{
+          flex: 1,
+        }}
+      >
+        <LinearGradient
+          colors={[
+            "rgba(255,255,255,0)",
+            "rgba(255,255,255,0.5)",
+            "white",
+            "white",
+          ]}
+          style={styles.gradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.8 }}
+        />
+      </Animated.View>
     </View>
   );
 };
@@ -19,10 +40,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
   bgImg: {
     width: wp(100),
     height: hp(100),
     position: "absolute",
+  },
+
+  gradient: {
+    width: wp(100),
+    height: hp(100),
+    position: "absolute",
+    bottom: 0,
   },
 });
 
