cabf315cde2c539b31e649df78615098740c84cd
Moazam05 <salmanmoazam08@gmail.com>
2024-07-13 16:36:25 +0500

    content of the welcome screen

--- app/index.js
+++ app/index.js
@@ -1,8 +1,16 @@
-import { View, Text, StatusBar, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StatusBar,
+  Image,
+  StyleSheet,
+  Pressable,
+} from "react-native";
 import React from "react";
 import { wp, hp } from "../helpers/common";
 import { LinearGradient } from "expo-linear-gradient";
 import Animated, { FadeInDown } from "react-native-reanimated";
+import { theme } from "../constants/theme";
 
 const WelcomeScreen = () => {
   return (
@@ -31,6 +39,17 @@ const WelcomeScreen = () => {
           start={{ x: 0.5, y: 0 }}
           end={{ x: 0.5, y: 0.8 }}
         />
+        {/* Content */}
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>Pixels</Text>
+          <Text style={styles.punchline}>Every Pixel Tells a Story</Text>
+
+          <View>
+            <Pressable style={styles.startButton}>
+              <Text style={styles.startText}>Start Explore</Text>
+            </Pressable>
+          </View>
+        </View>
       </Animated.View>
     </View>
   );
@@ -52,6 +71,42 @@ const styles = StyleSheet.create({
     height: hp(100),
     position: "absolute",
     bottom: 0,
+  },
+
+  contentContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    gap: 14,
+  },
+
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold,
+  },
+
+  punchline: {
+    fontSize: hp(2),
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: theme.fontWeights.medium,
+  },
+
+  startButton: {
+    marginBottom: 50,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 90,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+  },
+
+  startText: {
+    color: theme.colors.white,
+    fontWeight: theme.fontWeights.medium,
+    fontSize: hp(3),
+    letterSpacing: 1,
   },
 });
 
--- constants/theme.js
+++ constants/theme.js
@@ -2,22 +2,20 @@ export const theme = {
   colors: {
     white: "#fff",
     black: "#000",
-    grayBG: "#eSe5e5",
+    grayBG: "#e5e5e5",
     // neutral
-    neutral: (opacity = rgba(10, 10, 10, $(opacity))),
-
-    fontWeights: {
-      medium: "500",
-      semibold: "600",
-      bold: "700",
-    },
-
-    radius: {
-      xs: 10,
-      sm: 12,
-      md: 14,
-      lg: 16,
-      xl: 18,
-    },
+    neutral: (opacity) => `rgba(10, 10, 10, ${opacity})`,
+  },
+  fontWeights: {
+    medium: "500",
+    semibold: "600",
+    bold: "700",
+  },
+  radius: {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
   },
 };
