a833d5a6f11343b0e65feccd15fdf9533d0a0892
Moazam05 <salmanmoazam08@gmail.com>
2024-07-13 15:15:12 +0500

    helper function

---
+++ helpers/common.js
@@ -0,0 +1,15 @@
+import { Dimensions } from "react-native";
+
+const { width: deviceWidth, height: deviceHeight } = Dimensions.get("window");
+
+// width percentage
+export const wp = (percentage) => {
+  const width = deviceWidth;
+  return (percentage * width) / 100;
+};
+
+// height percentage
+export const hp = (percentage) => {
+  const height = deviceHeight;
+  return (percentage * height) / 100;
+};
