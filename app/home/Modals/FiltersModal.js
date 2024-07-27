d800957dc805690467be1384589a1820fb12a25a
Moazam05 <salmanmoazam08@gmail.com>
2024-07-27 16:08:14 +0500

    backdrop

--- app/home/Modals/FiltersModal.js
+++ app/home/Modals/FiltersModal.js
@@ -1,34 +1,61 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useMemo } from "react";
 import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+import { BlurView } from "expo-blur";
+import Animated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
 const FiltersModal = ({ modalRef }) => {
   const snapPoints = useMemo(() => ["75%"], []);
 
   return (
-    // <View style={styles.container}>
     <BottomSheetModal
       ref={modalRef}
       index={0}
       snapPoints={snapPoints}
       // onChange={handleSheetChanges}
       enablePanDownToClose={true}
+      backdropComponent={CustomBackdrop}
     >
       <BottomSheetView style={styles.contentContainer}>
         <Text>Awesome 🎉</Text>
       </BottomSheetView>
     </BottomSheetModal>
-    // </View>
+  );
+};
+
+const CustomBackdrop = ({ animatedIndex, style }) => {
+  const containerAutomatedStyle = useAnimatedStyle(() => {
+    let opacity = interpolate(
+      animatedIndex.value,
+      [-1, 0],
+      [0, 1],
+      Extrapolation.CLAMP
+    );
+
+    return {
+      opacity,
+    };
+  });
+
+  const containerStyle = [
+    StyleSheet.absoluteFillObject,
+    style,
+    { backgroundColor: "rgba(0,0,0,0.5)" },
+    containerAutomatedStyle,
+  ];
+
+  return (
+    <Animated.View style={containerStyle}>
+      <BlurView intensity={25} tint="dark" style={StyleSheet.absoluteFill} />
+    </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    justifyContent: "center",
-    backgroundColor: "grey",
-  },
   contentContainer: {
     flex: 1,
     alignItems: "center",
